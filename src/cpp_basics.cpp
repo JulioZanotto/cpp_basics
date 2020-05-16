9e9d4d96d802c50c6de630d0f93a0a3d3c299502
JulioZanotto <jczanotto@yahoo.com.br>
2020-05-15 21:57:13 -0300

    added features

--- src/cpp_basics.cpp
+++ src/cpp_basics.cpp
@@ -9,7 +9,19 @@
 #include <iostream>
 using namespace std;
 
+void printaNome(){
+	cout << "To de saca chamando funcao" << endl;
+}
+
+int retornaConta(int x, int y){
+	int soma;
+	soma = x + y;
+	return soma;
+}
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	printaNome();
+	cout << retornaConta(1,2) << endl;
 	return 0;
 }
